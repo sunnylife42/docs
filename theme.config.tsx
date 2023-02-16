2035d0d32bdfa711350cacdba102b6524ed0207f
Chuns Chen <i@chunschen.com>
2023-02-17 07:11:31 +0800

    feat: og title

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import {  DocsThemeConfig } from 'nextra-theme-docs'
+import {DocsThemeConfig, useConfig} from 'nextra-theme-docs'
 
 const title = '闪闪人生'
 const description = '拥抱爱与成就，就用闪闪人生'
@@ -24,9 +24,11 @@ const config: DocsThemeConfig = {
   },
   head: () => {
     const { asPath } = useRouter()
+    const config = useConfig()
     return (
       <>
         <meta name="og:url" content={`${url}${asPath}`} />
+        <meta name="og:title" content={config.title ? `${config.title} – ${title}` : title} />
         <meta name="og:image" content="https://cdn.sunnyhuang.net/share/logo-square.png" />
       </>
     )
